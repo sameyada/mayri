10102aed87776eb752e61ce09aa56c2402daa6b4
unknown <sameer.up78@gmail.com>
2025-07-23 20:27:33 +0530

    provider code

---
+++ main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.37.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "b397c2d0-369a-47dd-9100-9467b7447185"
+}
